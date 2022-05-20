eb92771a1fb356a179ac57599cf3363cff0293f6
Nahom Bekele <nahom.fix@gmail.com>
2022-05-20 23:36:32 +0300

    feat: add random forest model script

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,9 +1,9 @@
 import mlflow
-from config import Config
 import pandas as pd
 from sklearn.model_selection import KFold
-from evaluate_model import EvaluateModel
 
+from config import Config
+from evaluate_model import EvaluateModel
 
 Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
 
@@ -11,82 +11,77 @@ Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
 X_train = pd.read_csv(str(Config.FEATURES_PATH / "train_features.csv"))
 y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
 
-0
-class TrainModel():
-  '''
-  Class trains a model using 5-fold cross validation and returns the best model
-  '''
 
-  def __init__(self, model, name, params):
-    self.model = model
-    self.name = name
-    self.params = params
+class TrainModel:
+    """
+    Class trains a model using 5-fold cross validation and returns the best model
+    """
 
-  def get_avg_metics(self, metrics_list):
-    length = len(metrics_list)
+    def __init__(self, model, name, params):
+        self.model = model
+        self.name = name
+        self.params = params
 
-    return dict(
-        accuracy=sum([m["accuracy"] for m in metrics_list]) / length,
-        r_squared=sum([m["r_squared"] for m in metrics_list]) / length,
-        rmse=sum([m["rmse"] for m in metrics_list]) / length,
-        mae=sum([m["mae"] for m in metrics_list]) / length,
-    )
+    def get_avg_metics(self, metrics_list):
+        length = len(metrics_list)
 
-  def get_optimal_model(self):
-    if(self.name == 'XGBoost'):
-      mlflow.set_experiment('XGBoost')
-      mlflow.xgboost.autolog()
+        return dict(
+            accuracy=sum([m["accuracy"] for m in metrics_list]) / length,
+            r_squared=sum([m["r_squared"] for m in metrics_list]) / length,
+            rmse=sum([m["rmse"] for m in metrics_list]) / length,
+            mae=sum([m["mae"] for m in metrics_list]) / length,
+        )
 
-    elif(self.name == 'Decision Tree'):
-      mlflow.set_experiment('Decision Tree')
-      mlflow.sklearn.autolog()
+    def get_optimal_model(self):
+        if self.name == "XGBoost":
+            mlflow.set_experiment("XGBoost")
+            mlflow.xgboost.autolog()
 
-    elif(self.name=='Random forest'):
-      mlflow.set_experiment('Random forest')
-      mlflow.sklearn.autolog()
+        elif self.name == "Decision Tree":
+            mlflow.set_experiment("Decision Tree")
+            mlflow.sklearn.autolog()
 
-    else:
-      mlflow.set_experiment('Logistic Regression')
-      mlflow.sklearn.autolog()
-    return self.train_model()
+        elif self.name == "Random forest":
+            mlflow.set_experiment("Random forest")
+            mlflow.sklearn.autolog()
 
-  def train_model(self):
-    best_model = None
-    best_param = None
-    avg_metrics = dict(
-        accuracy=0,
-        r_squared=0,
-        rmse=0,
-        mae=0
-    )
+        else:
+            mlflow.set_experiment("Logistic Regression")
+            mlflow.sklearn.autolog()
+        return self.train_model()
 
-    for param in self.params:
-      new_model = self.model(param)
-      metrics = self.train(new_model, 5)
-      new_avg_metrics = self.get_avg_metics(metrics)
-      if(new_avg_metrics["accuracy"] > avg_metrics["accuracy"]):
-        best_model = new_model
-        best_param = param
-        avg_metrics = new_avg_metrics
+    def train_model(self):
+        best_model = None
+        best_param = None
+        avg_metrics = dict(accuracy=0, r_squared=0, rmse=0, mae=0)
 
-    return best_model, best_param, avg_metrics
+        for param in self.params:
+            new_model = self.model(param)
+            metrics = self.train(new_model, 5)
+            new_avg_metrics = self.get_avg_metics(metrics)
+            if new_avg_metrics["accuracy"] > avg_metrics["accuracy"]:
+                best_model = new_model
+                best_param = param
+                avg_metrics = new_avg_metrics
 
-  def train(self, new_model, num_split):
-    kf = KFold(n_splits=num_split)
-    randomIter = kf.split(X_train)
-    metrics_list = []
-    for i in range(5):
-      train_index, val_index = next(randomIter)
-      _X_train = X_train.iloc[train_index]
-      _y_train = y_train.iloc[train_index]
+        return best_model, best_param, avg_metrics
 
-      _X_val = X_train.iloc[val_index]
-      _y_val = y_train.iloc[val_index]
+    def train(self, new_model, num_split):
+        kf = KFold(n_splits=num_split)
+        randomIter = kf.split(X_train)
+        metrics_list = []
+        for i in range(5):
+            train_index, val_index = next(randomIter)
+            _X_train = X_train.iloc[train_index]
+            _y_train = y_train.iloc[train_index]
 
-      new_model.fit(_X_train, _y_train.to_numpy().ravel())
-      y_pred = new_model.predict(_X_val)
-      evaluate = EvaluateModel(_y_val, y_pred)
-      metrics = evaluate.get_metrics()
-      metrics_list.append(metrics)
+            _X_val = X_train.iloc[val_index]
+            _y_val = y_train.iloc[val_index]
 
-    return metrics_list
+            new_model.fit(_X_train, _y_train.to_numpy().ravel())
+            y_pred = new_model.predict(_X_val)
+            evaluate = EvaluateModel(_y_val, y_pred)
+            metrics = evaluate.get_metrics()
+            metrics_list.append(metrics)
+
+        return metrics_list
---
+++ scripts/train_random_forest.py
@@ -0,0 +1,27 @@
+import pickle
+
+from sklearn.ensemble import RandomForestClassifier
+
+from config import Config
+from train_model import TrainModel
+
+"""
+  simple script for training logistic regression using TrainModel class
+"""
+
+
+def model(param):
+    model = RandomForestClassifier(solver=param, random_state=42)
+    return model
+
+
+params = ["newton-cg", "lbfgs", "liblinear", "sag", "saga"]
+
+train_model = TrainModel(model, "Random forest", params=params)
+
+final_model, best_param, avg_metrics = train_model.get_optimal_model()
+
+pickle.dump(
+    final_model,
+    open(str(Config.MODELS_PATH / "random_forest_model.pickle"), "wb"),
+)
