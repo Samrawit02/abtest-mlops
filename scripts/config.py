0db71f7933e2f816f99dbe94bebf9c1479a69ea5
Samrawit02 <hairaer02@gmail.com>
2022-08-03 15:22:25 +0300

    updating  files

--- scripts/config.py
+++ scripts/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 class Config:
   RANDOM_SEED = 42
   ASSETS_PATH = Path("../")
-  REPO = "/Users/samrit/Documents/10Academy/week2/SmartAd contribution/abtest-ml/abtest-mlops"
+  REPO= "/Users/Samrit/Desktop/10 academy projects/abtest/abtest-mlops"
   DATASET_FILE_PATH = "data/AdSmartABdata.csv"
   DATASET_PATH = ASSETS_PATH / "data"
   FEATURES_PATH = ASSETS_PATH / "features"
